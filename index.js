a5c3b92607d74990e4fa3d4b0bd7771ae53a6020
mukarramahmed <mukarramkhan235@gmail.com>
2025-02-06 15:54:48 +0530

    data sharing

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let app=express()
 import dotenv from "dotenv"
 import bodyParser from "body-parser"
 import userRouter from "./routes/userRoute.js"
-import orderRouter from "./routes/orderRoute.js"
 import cors from "cors"
 import dbConnect from "./db/connection.js"
+import productRoute from "./routes/productRoute.js"
+
 
 
 
@@ -18,11 +19,12 @@ dbConnect(process.env.DBURL,process.env.DBNAME)
 app.use(bodyParser.json());
 app.use(express.json())
 
+app.use('/product', productRoute);
+
 
 app.use(cors())
 app.use('/user',userRouter)
-app.use('/order',orderRouter)
-app.use('/payment',orderRouter)
+
 
 
 
