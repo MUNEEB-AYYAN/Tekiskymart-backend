58ced94cda1448c2cba5afdad4f7fc3043aa3a15
akef <fkhanfkhan015@gmail.com>
2025-02-15 15:19:52 +0530

    created

--- models/productModel.js
+++ models/productModel.js
@@ -1,24 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
 
-  description: { type: String, required: true },
+    description: { type: String, required: true },
 
-  price: { type: Number, required: true },
+    price: { type: Number, required: true },
 
-  quantity: { type: Number, required: true },
+    quantity: { type: Number, required: true },
 
-  images:{
-    type:[String],require :true
+    images: { type: [String], require: true },
+
+    productCategory: { type:[String] , required: true },
   },
+  { timestamps: true }
+);
 
-  productcategory:{type:[String],require:true
+const Product = mongoose.model("Product", productSchema);
 
-  },
-  
-}, { timestamps: true });
-
-const Product = mongoose.model('Product', productSchema);
-
-export default Product
+export default Product;
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { createProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct, getAllProduct, getOneProduct, updateProduct,categorycontroller } from '../controllers/productController.js';
 let productRoute = express.Router()
 
 productRoute.post('/create', createProduct);
+productRoute.get('/getallproduct',getAllProduct);
+productRoute.put('/updateproduct/:id',updateProduct);
+productRoute.delete('/deleteproduct',deleteProduct);
+productRoute.get('/getOneProduct',getOneProduct)
+productRoute.get('/category',categorycontroller)
+
 
 export default productRoute
